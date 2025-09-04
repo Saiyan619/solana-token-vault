76255689f8f7cea36dc4c06322eda5dabc1d4ae1
olaniyi <arokoyueb11@gmail.com>
2025-09-04 10:52:51 +0100

    testing FE code on pc instead of wsl

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
                     {connected && <WalletDisconnectButton className="w-full" />}
                 </div>
 
+                {/* imposter */}
                 {/* Connection Status */}
                 <div className="mb-6">
                     <p className="mb-2">
