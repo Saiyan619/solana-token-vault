ab5355a86007aa9ebc193c023896cc13353f383a
olaniyi <arokoyueb11@gmail.com>
2025-09-15 09:56:51 +0100

    the end of an historic battle

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import path from 'path'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Load .env from parent folder
-  const env = loadEnv(mode, path.resolve(__dirname, '../'), '')
 
   return {
     plugins: [react()],
